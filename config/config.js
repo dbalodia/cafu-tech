5251a0e701669c8ac31b250e6998d6efd149df05
Dheeraj Balodia <dbalodia@gmail.com>
2020-02-06 17:42:54 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,23 +1,3 @@
-License Period: 17 Sep 2019 - 17 Mar 2020
-
-#Aw==\djF8MnwxfHRydWV8MTcwOTE5LzExOjQ1OjEwLjQ3NHwxNy4wOS4yMDE5fDE3LjAz
-LjIwMjB8c3ViLm5jfGRiYWxvZGlhQGdtYWlsLmNvbXxTdHVkaW8gM1QgTm9uLUNvbW1lcm
-NpYWwgTGljZW5zZXxf\iHk/R6BuQEvZEnufqi+g1pZYv2hjfgUL31pM6rwy3z/QjrCCLP9
-XVcUyPP/EFM7HDLf2b786bqu1R1Lz1+3qyC4pmQZ1F0vzZ9lpKlReTZ/lTfMH+98TDjp1V
-2Dqod9El4nB8ZUNItllhhDXAKTHT1/gO6k7xtClzyEO3uZ7gxh8wgmaOG9zNYbeVkBBwWs
-t+4yti34CTxiEWXmoHjzzms4uH88KYzA674B2ozoEwvhu2RqCzAep3AizevIiJofIsIGnK
-+g5jdHLWjwj2zSaJqAB8o1B2uLoZPebIdvv7wgt8QXRfmzRXLsZHp3iZLsOJex5+6q+XWu
-KTgrN/2O03w==#
----
-
-
-
-
-
-
-
-
-
 /* eslint-disable no-undef */
 import { argv } from 'yargs';
 import packageFile from './../package.json';
